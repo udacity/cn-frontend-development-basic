ecb9a33e67e3978ddb5a62c6615d563c452d4b72
evestorm <1572534835@qq.com>
2017-12-27 15:20:04 +0800

    add project memory-game

---
+++ memory-game_zh/README.md
@@ -0,0 +1,18 @@
+# 记忆游戏项目
+
+## 目录
+
+- [说明](#instructions)
+- [贡献](#contributing)
+
+## 说明
+
+记忆游戏初始项目中有一些 HTML 和 CSS 文件的静态版本。 你需要将此项目从静态项目转换为交互式项目。 这需要修改 HTML 和 CSS 文件，但主要是对 JavaScript 文件的编写。
+
+首先，请打开`js/app.js`并开始构建应用程序的功能。
+
+有关详细说明，请查看[优达学城教室](https://classroom.udacity.com/me)中的项目说明。
+
+## 贡献
+
+此仓库是 _所有_ 优达学城学生的入门代码。 因此，我们很可能不会接受 pull requests 请求。有关详细信息，请查看 [CONTRIBUTING.md](CONTRIBUTING.md)。
---
+++ memory-game_zh/js/app.js
@@ -0,0 +1,38 @@
+/*
+ * 创建一个包含所有卡片的列表
+ */
+
+
+/*
+ * 显示页面上的卡片
+ *   - 使用下面提供的 “shuffle” 方法对列表中的卡片进行洗牌
+ *   - 循环遍历每张卡片，创建其 HTML
+ *   - 将每张卡的 HTML 添加到页面
+ */
+
+// 洗牌函数来自于 http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * 设置一张卡片的事件监听器。 如果该卡片被点击：
+ *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 将卡片添加到状态为 “open” 的 *列表* 中（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 如果列表中已有另一张卡，请检查两张卡片是否匹配
+ *    + 如果卡片匹配，将卡片锁定为 ”open“ 状态（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果卡片不匹配，请将卡片从列表中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果所有卡都匹配，则显示带有最终分数的消息 将这个功能放在你从这个函数中调用的另一个函数中）
+ */
